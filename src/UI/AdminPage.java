359cd4286f4962419e91097b12f9bfa3419438e5
ChunyanLi551 <li.chunya@northeastern.edu>
2026-04-02 19:54:57 -0400

    Update AdminPage.java

--- src/UI/AdminPage.java
+++ src/UI/AdminPage.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.util.List;
 import java.util.Stack;
 
 import javax.swing.JButton;
