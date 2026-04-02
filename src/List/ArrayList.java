2678980b384d055490ab112992b5b0adc2047a1a
alec589 <sun.zuo@northeastern.edu>
2026-04-01 21:13:18 -0400

    listinterface arraylist implementation

--- src/List/ArrayList.java
+++ src/List/ArrayList.java
@@ -1,5 +1,112 @@
 package List;
 
-public class ArrayList {
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
+public class ArrayList<T> implements ListInterface<T> {
+    private T[] list;
+    private int numberOfEntries;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    @SuppressWarnings("unchecked")
+    public ArrayList() {
+        list = (T[]) new Object[DEFAULT_CAPACITY];
+        numberOfEntries = 0;
+    }
+
+    @Override
+    public void add(T newEntry) {
+        checkCapacity();
+        list[numberOfEntries] = newEntry;
+        numberOfEntries++;
+    }
+
+    @Override
+    public T get(int index) {
+        checkIndex(index);
+        return list[index];
+    }
+
+    @Override
+    public T set(int index, T newEntry) {
+        checkIndex(index);
+        T oldEntry = list[index];
+        list[index] = newEntry;
+        return oldEntry;
+    }
+
+    @Override
+    public T remove(int index) {
+        checkIndex(index);
+        T removed = list[index];
+
+        for (int i = index; i < numberOfEntries - 1; i++) {
+            list[i] = list[i + 1];
+        }
+
+        list[numberOfEntries - 1] = null;
+        numberOfEntries--;
+        return removed;
+    }
+
+    @Override
+    public int size() {
+        return numberOfEntries;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return numberOfEntries == 0;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < numberOfEntries; i++) {
+            list[i] = null;
+        }
+        numberOfEntries = 0;
+    }
+//Check if array is full;if full, create a new array with double size
+    private void checkCapacity() {
+        if (numberOfEntries >= list.length) {
+            @SuppressWarnings("unchecked")
+            T[] doubleSize = (T[]) new Object[list.length * 2];
+            for (int i = 0; i < list.length; i++) {
+            	doubleSize[i] = list[i];
+            }
+            list = doubleSize;
+        }
+    }
+ // Make sure index is within valid range
+    private void checkIndex(int index) {
+        if (index < 0 || index >= numberOfEntries) {
+            throw new IndexOutOfBoundsException("Invalid index: " + index);
+        }
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ListIterator();
+    }
+
+    /**
+     * Inner class that implements Iterator
+     */
+    private class ListIterator implements Iterator<T> {
+
+        private int nextIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            return nextIndex < numberOfEntries;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return list[nextIndex++];
+        }
+    }
 }
--- src/List/ListInterface.java
+++ src/List/ListInterface.java
@@ -1,5 +1,67 @@
 package List;
 
-public interface ListInterface {
+import java.util.Iterator;
 
+/**
+ * A simple list interface.
+ * Stores elements in order and allows basic operations.
+ *
+ * @param <T> type of elements
+ */
+public interface ListInterface<T> extends Iterable<T> {
+
+    /**
+     * Add an element to the end of the list
+     * @param newEntry    the element to be added
+     */
+	public void add(T newEntry);
+
+    /**
+     * Get element at given index
+     *  @param index   the position of the element 
+     *  @throws IndexOutOfBoundsException if index is out of range
+     */
+	public T get(int index);
+
+    /**
+     * Replace element at index
+     * @param index the position of the element
+     * @param newEntry the new element to replace the old one
+     * @return the element previously at the specified position
+     * @throws IndexOutOfBoundsException if index is out of range
+     */
+	public T set(int index, T newEntry);
+
+    /**
+     * Remove element at index
+     * @param index the position of the element
+     * @return the removed element
+     * @throws IndexOutOfBoundsException if index is out of range
+     */
+	public T remove(int index);
+
+    /**
+     * Get number of elements
+     * 
+     * @return the size of the list
+     */
+	public int size();
+
+    /**
+     * Check if list is empty
+     * @return true if the list contains no elements, false otherwise
+     */
+	public boolean isEmpty();
+
+    /**
+     * Remove all elements
+     */
+	public void clear();
+
+    /**
+     * Return iterator for loop
+     * @return an iterator for traversing the list
+     */
+	
+    Iterator<T> iterator();
 }
